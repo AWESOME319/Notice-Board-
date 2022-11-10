347274844940401c3a90e6283639b6fd7952e627
AWESOME319 <83673925+AWESOME319@users.noreply.github.com>
2022-11-10 11:52:36 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,39 +1,31 @@
-noseX=0;
-noseY=0;
 difference=0;
 rightWristX=0;
 leftWristX=0;
 function setup(){
     video=createCapture(VIDEO);
-    video.size(550,500)
+    video.size(550,500);
     canvas=createCanvas(550,450);
-    canvas.position(560,100);
-    poseNet=ml5.poseNet(video,modelLoaded);
+    canvas.position(560,70);
+    poseNet= ml5.poseNet(video,modelLoaded);
     poseNet.on('pose',gotPoses);
+    textSize(difference)
 }
 function draw(){
-    background('#969A97');
-    document.getElementById("square_side").innerHTML="Width And Height of Square will be:"+ difference +"px";
-    fill('#F90093');
-    stroke('#F90093');
-    square(noseX,noseY,difference);
-}
-
+    background('#2a7ce8');
+    fill('#050505');
+    text('This month many things will be happening. We have a food drive which starts on November 14.',20,100);
+ }
 function modelLoaded(){
-    console.log('poseNet is Initialized')
+    console.log('PoseNet is initialized');
 }
 function gotPoses(results)
 {
     if(results.length > 0)
     {
-        console.log(results);
-        noseX=results[0].pose.nose.x;
-        noseY=results[0].pose.nose.y;
-        console.log("noseX="+noseX+"noseY ="+noseY);
-
+        console.log(results)
         leftWristX=results[0].pose.leftWrist.x;
-       rightWristX=results[0].pose.rightWrist.x;
-       difference=floor(leftWristX-rightWristX);
-       console.log("leftWristX = " + leftWristX + "rightWristX" + rightWristX +"difference = " + difference)
+        rightWristX=results[0].pose.rightWrist.x;
+        difference=floor(leftWristX-rightWristX);
+        console.log("leftWristX = " + leftWristX + "rightWristX" + rightWristX +"difference = " + difference)
     }
 }
